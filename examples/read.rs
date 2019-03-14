2d24b03b426194d2f35118d16932f1aad4142e6f
Jeremy Soller <jeremy@system76.com>
2019-03-13 20:34:58 -0600

    Support read operation

---
+++ examples/read.rs
@@ -0,0 +1,47 @@
+extern crate libc;
+extern crate intel_spi;
+
+use intel_spi::{Spi, SpiCnl};
+use std::{fs, mem, ptr};
+
+unsafe fn get_spi() -> &'static mut SpiCnl {
+    let spibar = 0xfe010000;
+
+    let fd = libc::open(
+        b"/dev/mem\0".as_ptr() as *const libc::c_char,
+        libc::O_RDWR
+    );
+    if fd < 0 {
+        panic!("failed to open /dev/mem");
+    }
+
+    let p = libc::mmap(
+        ptr::null_mut(),
+        mem::size_of::<SpiCnl>(),
+        libc::PROT_READ | libc::PROT_WRITE,
+        libc::MAP_SHARED,
+        fd,
+        spibar
+    );
+    if p == libc::MAP_FAILED {
+        panic!("failed to map /dev/mem");
+    }
+
+    &mut *(p as *mut SpiCnl)
+}
+
+fn main() {
+    let spi = unsafe { get_spi() };
+
+    let hsfsts_ctl = spi.hsfsts_ctl();
+    println!("{:X}: {:?}", hsfsts_ctl.bits(), hsfsts_ctl);
+
+    let len = spi.len().unwrap();
+    println!("SPI ROM: {} KB", len / 1024);
+
+    let mut data = vec![0; len];
+    let read = spi.read(0, &mut data).unwrap();
+    println!("SPI READ: {} KB", read / 1024);
+
+    fs::write("dump.rom", &data).unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 #[macro_use]
 extern crate bitflags;
 
+use core::mem;
+
 pub use self::io::Io;
 mod io;
 
@@ -12,8 +14,11 @@ mod mmio;
 
 #[derive(Debug)]
 pub enum SpiError {
+    /// Access Error Log (H_AEL) is set
     Access,
+    /// Flash Cycle Error (FCERR) is set
     Cycle,
+    /// Register contains unexpected data
     Register,
 }
 
@@ -25,67 +30,246 @@ pub trait Spi {
 
 bitflags! {
     pub struct HsfStsCtl: u32 {
+        /// Flash Cycle Done
         const FDONE = 1 << 0;
+        /// Flash Cycle Error
         const FCERR = 1 << 1;
+        /// Access Error Log
         const H_AEL = 1 << 2;
+
+        // Reserved 3:4
+
+        /// SPI Cycle In Progress
         const H_SCIP = 1 << 5;
+
+        // Reserved 6:10
+
+        /// Write Status Disable
         const WRSDIS = 1 << 11;
+        /// PRR3 PRR4 Lock-Down
         const PRR34_LOCKDN = 1 << 12;
+        /// Flash Descriptor Override Pin-Strap Status
         const FDOPSS = 1 << 13;
+        /// Flash Descriptor Valid
         const FDV = 1 << 14;
+        /// Flash Configuration Lock-Down
         const FLOCKDN = 1 << 15;
+        //// Flash Cycle Go
         const FGO = 1 << 16;
+
+        /// Flash Cycle
         const FCYCLE = 0b1111 << 17;
+        const FCYCLE_0 = 1 << 17;
+        const FCYCLE_1 = 1 << 18;
+        const FCYCLE_2 = 1 << 19;
+        const FCYCLE_3 = 1 << 20;
+
+        /// Write Enable Type
         const WET = 1 << 21;
+
+        // Reserved 22:23
+
+        /// Flash Data Byte Count - Programmed with count minus one
         const FDBC = 0b111111 << 24;
+        const FDBC_0 = 1 << 24;
+        const FDBC_1 = 1 << 25;
+        const FDBC_2 = 1 << 26;
+        const FDBC_3 = 1 << 27;
+        const FDBC_4 = 1 << 28;
+        const FDBC_5 = 1 << 29;
+
+        // Reserved 30
+
+        /// Flash SPI SMI# Enable
         const FSMIE = 1 << 31;
     }
 }
 
+impl HsfStsCtl {
+    fn cycle(&self) -> HsfStsCtlCycle {
+        unsafe { mem::transmute((*self & Self::FCYCLE).bits) }
+    }
+
+    fn set_cycle(&mut self, value: HsfStsCtlCycle) {
+        *self = (*self & !Self::FCYCLE) | (
+            Self::from_bits_truncate(value as u32) &
+            Self::FCYCLE
+        );
+    }
+
+    fn count(&self) -> u8 {
+        (((*self & Self::FDBC).bits >> 24) + 1) as u8
+    }
+
+    fn set_count(&mut self, value: u8) {
+        *self = (*self & !Self::FDBC) | (
+            Self::from_bits_truncate((value.saturating_sub(1) as u32) << 24) &
+            Self::FDBC
+        );
+    }
+}
+
+#[repr(u32)]
+pub enum HsfStsCtlCycle {
+    /// Read number of bytes in FDBC plus one
+    Read = 0x0 << 17,
+    /// Reserved - treated as 0x0 read
+    Rsvd = 0x1 << 17,
+    /// Write number of bytes in FDBC plus one
+    Write = 0x2 << 17,
+    /// Erase 4096 bytes
+    BlockErase = 0x3 << 17,
+    /// Erase 65536 bytes
+    SectorErase = 0x4 << 17,
+    /// Read SFDP
+    ReadSfdp = 0x5 << 17,
+    /// Read JEDEC ID
+    ReadJedec = 0x6 << 17,
+    /// Write status
+    WriteStatus = 0x7 << 17,
+    /// Read status
+    ReadStatus = 0x8 << 17,
+    /// RPMC Op1
+    RpmcOp1 = 0x9 << 17,
+    /// Rpmc Op2
+    RpmcOp2 = 0xA << 17,
+}
+
+#[repr(u32)]
+pub enum FdoSection {
+    Map = 0b000 << 12,
+    Component = 0b001 << 12,
+    Region = 0b010 << 12,
+    Master = 0b011 << 12
+}
+
+#[allow(dead_code)]
 #[repr(packed)]
 pub struct SpiCnl {
+    /// BIOS Flash Primary Region
     bfpreg: Mmio<u32>,
+    /// Hardware Sequencing Flash Status and Control
     hsfsts_ctl: Mmio<u32>,
+    /// Flash Address
     faddr: Mmio<u32>,
+    /// Discrete Lock Bits
     dlock: Mmio<u32>,
+    /// Flash Data
     fdata: [Mmio<u32>; 16],
+    /// Flash Region Access Permissions
     fracc: Mmio<u32>,
+    /// Flash Regions
     freg: [Mmio<u32>; 6],
     _reserved1: [Mmio<u32>; 6],
+    /// Flash Protected Ranges
     fpr: [Mmio<u32>; 5],
+    /// Global Protected Range
     gpr: Mmio<u32>,
     _reserved2: [Mmio<u32>; 5],
+    /// Secondary Flash Region Access Permissions
     sfracc: Mmio<u32>,
+    /// Flash Descriptor Observability Control
     fdoc: Mmio<u32>,
+    /// Flash Descriptor Observability Data
     fdod: Mmio<u32>,
     _reserved3: Mmio<u32>,
+    // Additional Flash Control
     afc: Mmio<u32>,
+    /// Vendor Specific Capabilities for Component 0
     vscc0: Mmio<u32>,
+    /// Vendor Specific Capabilities for Component 1
     vscc1: Mmio<u32>,
+    /// Parameter Table Index
     ptinx: Mmio<u32>,
+    /// Parameter Table Data
     ptdata: Mmio<u32>,
+    /// SPI Bus Requester Status
     sbrs: Mmio<u32>,
 }
 
-impl SpiCnl {        
-    pub fn hsfsts_ctl(&self) -> Result<HsfStsCtl, SpiError> {
-        HsfStsCtl::from_bits(self.hsfsts_ctl.read()).ok_or(SpiError::Register)
+impl SpiCnl {
+    pub fn hsfsts_ctl(&self) -> HsfStsCtl {
+        HsfStsCtl::from_bits_truncate(self.hsfsts_ctl.read())
     }
 
-    pub fn wait(&mut self) -> Result<(), SpiError> {
-        // Wait for cycle in progress
-        while self.hsfsts_ctl()?.contains(HsfStsCtl::H_SCIP) {}
-        Ok(())
+    pub fn set_hsfsts_ctl(&mut self, value: HsfStsCtl) {
+        self.hsfsts_ctl.write(value.bits);
+    }
+
+    pub fn fdo(&mut self, section: FdoSection, index: u16) -> u32 {
+        self.fdoc.write(
+            (section as u32) |
+            (((index & 0b1111111111) as u32) << 2)
+        );
+        self.fdod.read()
     }
 }
 
 impl Spi for SpiCnl {
     fn len(&mut self) -> Result<usize, SpiError> {
-        Ok(0)
+        let component = self.fdo(FdoSection::Component, 0);
+        Ok(match component & 0b111 {
+            0b000 => 512,
+            0b001 => 1024,
+            0b010 => 2048,
+            0b011 => 4096,
+            0b100 => 8192,
+            0b101 => 16384,
+            _ => return Err(SpiError::Register)
+        } * 1024)
     }
 
     fn read(&mut self, address: usize, buf: &mut [u8]) -> Result<usize, SpiError> {
-        Ok(0)
+        let mut count = 0;
+        for chunk in buf.chunks_mut(64) {
+            let mut hsfsts_ctl;
+
+            // Wait for other transactions
+            loop {
+                hsfsts_ctl = self.hsfsts_ctl();
+                if ! hsfsts_ctl.contains(HsfStsCtl::H_SCIP) {
+                    break;
+                }
+            }
+
+            hsfsts_ctl.set_cycle(HsfStsCtlCycle::Read);
+            hsfsts_ctl.insert(HsfStsCtl::FGO);
+
+            // Start command
+            self.faddr.write((address + count) as u32);
+            self.set_hsfsts_ctl(hsfsts_ctl);
+
+            // Wait for command to finish
+            loop {
+                hsfsts_ctl = self.hsfsts_ctl();
+                if ! hsfsts_ctl.contains(HsfStsCtl::H_SCIP) {
+                    break;
+                }
+            }
+
+            if hsfsts_ctl.contains(HsfStsCtl::FCERR) {
+                return Err(SpiError::Cycle);
+            }
+
+            if hsfsts_ctl.contains(HsfStsCtl::H_AEL) {
+                return Err(SpiError::Access);
+            }
+
+            if ! hsfsts_ctl.contains(HsfStsCtl::FDONE) {
+                return Err(SpiError::Register);
+            }
+
+            for (i, dword) in chunk.chunks_mut(4).enumerate() {
+                let mut data = self.fdata[i].read();
+                for byte in dword.iter_mut() {
+                    *byte = data as u8;
+                    data = data >> 8;
+                }
+            }
+
+            count += chunk.len()
+        }
+        Ok(count)
     }
 }
 
