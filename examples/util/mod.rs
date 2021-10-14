87ce46653294f33775893dd7cd8ff4be0f7993f2
Tim Crawford <tcrawford@system76.com>
2021-10-14 07:24:22 -0600

    Add SPDX license identifiers
    Add license information to files. Done with:
    
        git ls-files src/ examples/ | xargs sed -i '1s,^,// SPDX-License-Identifier: MIT\n\n,'
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- examples/util/mod.rs
+++ examples/util/mod.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 use intel_spi::SpiSkl;
 
 use std::{mem, ptr};
--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 use core::cmp::PartialEq;
 use core::ops::{BitAnd, BitOr, Not};
 
